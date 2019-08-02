5c260aff5c65ceb8ad0c4cf71bdb5f152f5ba582
Eric Butler <eric@level.com>
2019-08-02 17:24:13 -0400

    Added support for custom markers

--- src/Marker.js
+++ src/Marker.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
-import { Marker } from 'react-google-maps';
+import React, { Component } from "react";
+import { Marker, OverlayView } from "react-google-maps";
+
+const ConditionalWrap = ({ condition, wrap, children }) =>
+  condition ? wrap(children) : <React.Fragment>{children}</React.Fragment>;
 
 class MapViewMarker extends Component {
   state = {
-    isOpen: false,
+    isOpen: false
   };
   showCallout() {
     this.setState({ isOpen: true });
@@ -12,17 +15,47 @@ class MapViewMarker extends Component {
     this.setState({ isOpen: false });
   }
   render() {
-    const { description, title, coordinate, onPress, ...rest } = this.props;
+    const { description, title, coordinate, children, ...rest } = this.props;
+    if (children) {
+      return (
+        <OverlayView
+          {...rest}
+          title={description ? `${title}\n${description}` : title}
+          position={{ lat: coordinate.latitude, lng: coordinate.longitude }}
+          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+        >
+          <ConditionalWrap
+            condition={!!onPress}
+            wrap={c => (
+              <div
+                ref={ref =>
+                  ref && google.maps.OverlayView.preventMapHitsFrom(ref)
+                }
+                onClick={onPress}
+              >
+                {c}
+              </div>
+            )}
+          >
+            {children}
+          </ConditionalWrap>
+        </OverlayView>
+      );
+    }
 
     const childrenWithProps = React.Children.map(this.props.children, child => {
-      return React.cloneElement(child, { hideCallout: this.hideCallout.bind(this) });
+      return React.cloneElement(child, {
+        hideCallout: this.hideCallout.bind(this)
+      });
     });
+
     return (
       <Marker
         {...rest}
         title={description ? `${title}\n${description}` : title}
         position={{ lat: coordinate.latitude, lng: coordinate.longitude }}
-        onClick={onPress}>
+        onClick={onPress}
+      >
         {this.state.isOpen && childrenWithProps}
       </Marker>
     );
